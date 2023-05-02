66a8e3f177187672364b56d19b1a73f2cd48f663
Piri <a.ilin@yadro.com>
2023-05-02 18:48:39 +0300

    timer instead of time + websocket data exchange correction

--- src/backend/api/websocket_api.py
+++ src/backend/api/websocket_api.py
@@ -1,7 +1,9 @@
 import time
 import json
+import asyncio
 
 from fastapi import WebSocket, HTTPException
+from starlette.websockets import WebSocketState
 from fastapi import APIRouter
 
 from main import logger
@@ -9,25 +11,29 @@ from schemas import websocket_schemas
 
 api_router = APIRouter()
 
-
 @api_router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     logger.success('Websocket connection accept')
-    while True:
+    logger.info('NEW CONNECTION')
+    timer = 0
+    while websocket.client_state == WebSocketState.CONNECTED:
         try:
-            recieve_text = await websocket.receive_text()
-            recieve_object= json.loads(recieve_text)
-            try:
-                websocket_schemas.WebsocketInput(**recieve_object)
-            except ValueError:
-                logger.error('Bad ws message')
-                raise HTTPException(
-                    status_code=400, detail='Bad ws message'
-                    )
-            resp = {'timer': time.time()}
+            logger.info("SENDING DATA TO FRONT")
+            #recieve_text = await websocket.receive_text()
+            #recieve_object= json.loads(recieve_text)
+            # try:
+            #     websocket_schemas.WebsocketInput(**recieve_object)
+            # except ValueError:
+            #     logger.error('Bad ws message')
+            #     raise HTTPException(
+            #         status_code=400, detail='Bad ws message'
+            #         )
+            resp = {'timer': timer}
             await websocket.send_json(resp)
         except Exception as e:
             logger.error(f"Websocket error: {e}")
             break
-    logger.info('Connection refused')
+        await asyncio.sleep(1)
+        timer += 1
+    logger.info('Connection ended')
